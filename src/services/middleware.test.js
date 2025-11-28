7647bc0440917597df676856cb1384beb365b7b9
Kyle Willett <kylewillett@yahoo.com>
2025-11-27 20:50:47 -0600

    Updated test to use nested describe blocks

--- src/services/middleware.test.js
+++ src/services/middleware.test.js
@@ -7,17 +7,22 @@ afterAll((done) => {
     });
 });
 
-describe("validateId when id param is not a valid uuid", () => {
-    it("should return a 'Bad Request' response", async () => {
-        const res = await request(app).get("/api/articles/not-a-uuid");
-        expect(res.statusCode).toEqual(400);
-        expect(res.body.message).toBe("Article id must be a valid uuid");
-    });
-});
+describe("validateId", () => {
 
-describe("validateId when id param is a valid uuid", () => {
-    it("should invoke NextFunction which returns a 'Not Found' response", async () => {
-        const res = await request(app).get("/api/articles/0720f1cd-f42c-4122-adb2-6a904298a3e9");
-        expect(res.statusCode).toEqual(404);
+    describe("when id param is not a valid uuid", () => {
+        it("should return a 'Bad Request' response", async () => {
+            const res = await request(app).get("/api/articles/not-a-uuid");
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.message).toBe("Article id must be a valid uuid");
+        });
     });
-});
+
+    describe("when id param is a valid uuid", () => {
+        it("should invoke NextFunction which returns a 'Not Found' response", async () => {
+            const res = await request(app).get("/api/articles/0720f1cd-f42c-4122-adb2-6a904298a3e9");
+            expect(res.statusCode).toEqual(404);
+        });
+    });
+})
+
+
