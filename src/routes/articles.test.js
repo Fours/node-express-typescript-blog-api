f8cd25fe53f9d6b188bf4236a9705e4e2feab1ac
Kyle Willett <kylewillett@yahoo.com>
2025-11-30 14:03:01 -0600

    Added a POST test and updated tests to use real data store

--- src/routes/articles.test.js
+++ src/routes/articles.test.js
@@ -6,40 +6,30 @@ const testData = [
     {
         id: "ef70595d-260e-459f-bdaa-66d97b880775",
         timestamp: 1763670293754,
-        author: "test author 1",
-        tags: ["test tag 1"],
-        title: "test title 1",
-        blurb: "test blurb 1",
-        body: "test body 1"
+        author: "author 1",
+        tags: ["tag 1"],
+        title: "title 1",
+        blurb: "blurb 1",
+        body: "body 1"
     },
     {
         id: "dc4351e3-d12b-4018-a5dd-96eea6d788c4",
         timestamp: 1763670293754,
-        author: "test author 2",
-        tags: ["test tag 2"],
-        title: "test title 2",
-        blurb: "test blurb 2",
-        body: "test body 2"
+        author: "author 2",
+        tags: ["tag 2"],
+        title: "title 2",
+        blurb: "blurb 2",
+        body: "body 2"
     }
 ]
 
 const notAnArticleId = "d2549eec-0490-41ba-a4fd-4e45ce96ef10"
 
-let readDataSpy
-let writeDataSpy
-
-beforeAll((done) => {
-    readDataSpy = jest.spyOn(dataUtil, 'readData').mockReturnValue(Promise.resolve(testData));
-    writeDataSpy = jest.spyOn(dataUtil, 'writeData').mockReturnValue(Promise.resolve());
-    done();
+beforeEach(() => {    
+    dataUtil.writeData(testData)
 })
 
-afterEach(() => {
-  jest.clearAllMocks();
-});
-
 afterAll((done) => {
-    jest.resetAllMocks()
     app.close(() => {
         done();
     });
@@ -51,13 +41,8 @@ describe("GET /api/articles", () => {
         it("should return articles with an 'Ok' response", async () => {
             
             const res = await request(app).get("/api/articles/");
-            expect(res.statusCode).toEqual(200);
-            expect(res.body.length).toBe(2);
-            expect(res.body[0].id).toBe(testData[0].id);
-            expect(res.body[1].id).toBe(testData[1].id);
-
-            expect(readDataSpy).toHaveBeenCalledTimes(1);
-            expect(writeDataSpy).toHaveBeenCalledTimes(0);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toEqual(testData)
         });
     });
 })
@@ -68,12 +53,8 @@ describe("GET /api/articles/:id", () => {
         it("should return the article with an 'Ok' response", async () => {
             
             const res = await request(app).get(`/api/articles/${testData[1].id}`);
-            expect(res.statusCode).toEqual(200);
-            expect(res.body.id).toBe(testData[1].id);
-            expect(res.body.author).toBe(testData[1].author);
-
-            expect(readDataSpy).toHaveBeenCalledTimes(1);
-            expect(writeDataSpy).toHaveBeenCalledTimes(0);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toEqual(testData[1]);
         });
     });
 
@@ -81,13 +62,38 @@ describe("GET /api/articles/:id", () => {
         it("should return a 'Not Found' response", async () => {
             
             const res = await request(app).get(`/api/articles/${notAnArticleId}`);
-            expect(res.statusCode).toEqual(404);
+            expect(res.statusCode).toBe(404);
             expect(res.body.message).toBe("No article found with that id");
-
-            expect(readDataSpy).toHaveBeenCalledTimes(1);
-            expect(writeDataSpy).toHaveBeenCalledTimes(0);
         });
     });
+})
+
+describe("POST /api/articles", () => {
+
+    const articlePayload = {
+        author: "author 3",
+        tags: ["tag 3"],
+        title: "title 3",
+        blurb: "blurb 3",
+        body: "body 3"
+    }
+
+    describe("when new article is valid", () => {
+        it("should save article and return a 'Created' response", async () => {
+            
+            const res = await request(app).post("/api/articles/").send(articlePayload);
+            expect(res.statusCode).toBe(201);
+            expect(typeof res.body.id).toBe("string");
+            expect(res.body.id.length).toBe(36);
+            expect(typeof res.body.timestamp).toBe("number");            
+            const newArticle = {...articlePayload, id: res.body.id, timestamp: res.body.timestamp}
+            expect(res.body).toEqual(newArticle)
+
+            const updatedData = await dataUtil.readData()
+            expect(updatedData).toEqual([...testData, newArticle])
+        });
+    });
+
 })
 
 describe("DELETE /api/articles/:id", () => {
@@ -96,13 +102,11 @@ describe("DELETE /api/articles/:id", () => {
         it("should delete article and return an 'Ok' response", async () => {
             
             const res = await request(app).delete(`/api/articles/${testData[0].id}`);
-            expect(res.statusCode).toEqual(200);
+            expect(res.statusCode).toBe(200);
             expect(res.body.message).toBe("Article deleted");
 
-            expect(readDataSpy).toHaveBeenCalledTimes(1);
-            expect(writeDataSpy).toHaveBeenCalledTimes(1);
-            expect(writeDataSpy.mock.calls[0][0].length).toBe(1)
-            expect(writeDataSpy.mock.calls[0][0][0].id).toBe(testData[1].id)
+            const updatedData = await dataUtil.readData()
+            expect(updatedData).toEqual([testData[1]])
         });
     });
 
@@ -110,12 +114,8 @@ describe("DELETE /api/articles/:id", () => {
         it("should return a 'Not Found' response", async () => {
             
             const res = await request(app).delete(`/api/articles/${notAnArticleId}`);
-            expect(res.statusCode).toEqual(404);
+            expect(res.statusCode).toBe(404);
             expect(res.body.message).toBe("No article found with that id");
-
-            expect(readDataSpy).toHaveBeenCalledTimes(1);
-            expect(writeDataSpy).toHaveBeenCalledTimes(0);
         });
     });
-
 })
